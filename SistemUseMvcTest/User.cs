be22e1fe22156c1cded1b49c3d60b05b2c3e16b9
Claudemir <claus-santos80@hotmail.com>
2025-09-09 17:51:00 -0300

    Data anotations

--- SistemUseMvcTest/User.cs
+++ SistemUseMvcTest/User.cs
@@ -1,10 +1,25 @@
-﻿namespace SistemUseMvcTest
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemUseMvcTest
 {
     public class User
     {
+
+        [Display(Name = "Codigo do Usuário")]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Nome do Usuário")]
         public string? Name { get; set; }
+
+
+        [Required(ErrorMessage = "O nome do usuário é obrigatório", AllowEmptyStrings = false)]
+        [Display(Name = "Sobre  Nome do Usuário")]
         public string? username { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o seu email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Informe um email válido...")]
         public string? email  { get; set; }
 
 
